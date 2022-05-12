37d87689536fa2cd316afd1a58170850cf493229
mti <mti@purdue.edu>
2022-05-12 10:33:23 -0400

    fixed bugs

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return gitMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.teal,
