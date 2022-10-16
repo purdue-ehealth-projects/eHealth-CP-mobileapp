8c6750a78e0dc5397cb1e984b5d1fb31642a187a
mti <mti@purdue.edu>
2022-10-16 15:58:19 -0400

    fix changes (dropdown menu)

--- lib/survey_data.dart
+++ lib/survey_data.dart
@@ -13,53 +13,58 @@ List<String> questions = [
   "I feel like I need: \nChoose one option."
 ];
 
-List<String> choices1 = [
-  "I am breathing well",
-  "I run out of breath when walking",
-  "I run out of breath when talking"
-];
-
-List<String> choices2 = [
-  "Better than yesterday",
-  "Same as yesterday",
-  "Worse than yesterday"
-];
-
-List<String> choices3 = [
-  "I feel great",
-  "My heart is racing",
-  "I have chest pain"
-];
-
-List<String> choices4 = [
-  "I slept well last night",
-  "My sleep was restless",
-  "I had to use pillows to sit up in bed"
-];
-
-List<String> choices5 = [
-  'My weight went up 2 or more pounds since yesterday',
-  'My clothes and shoes feel tight',
-  'I can see swelling in my ankles',
-  'My weight is the same or less than yesterday'
-];
-
-List<String> choices6 = [
-  'More than yesterday',
-  'Same as yesterday',
-  'Less than yesterday'
-];
-
-List<String> choices7 = [
-  'Aching all over',
-  'A headache',
-  'Nausea',
-  'A new concern or problem that came back',
-  'None of the above',
-];
-
-List<String> choices8 = [
-  'A phone call',
-  'A visit',
-  'Nothing! I\'m good today :)'
+List<List<String>> choicesData = [
+  [
+    "I am breathing well",
+    "I run out of breath when walking",
+    "I run out of breath when talking"
+  ],
+  [
+    "Better than yesterday",
+    "Same as yesterday",
+    "Worse than yesterday"
+  ],
+  [
+    "I feel great",
+    "My heart is racing",
+    "I have chest pain"
+  ],
+  [
+    "I slept well last night",
+    "My sleep was restless",
+    "I had to use pillows to sit up in bed"
+  ],
+  [
+    "Better than yesterday",
+    "Same as yesterday",
+    "Worse than yesterday"
+  ],
+  [
+    'My weight went up 2 or more pounds since yesterday',
+    'My clothes and shoes feel tight',
+    'I can see swelling in my ankles',
+    'My weight is the same or less than yesterday'
+  ],
+  [
+    'More than yesterday',
+    'Same as yesterday',
+    'Less than yesterday'
+  ],
+  [
+    'Aching all over',
+    'A headache',
+    'Nausea',
+    'A new concern or problem that came back',
+    'None of the above'
+  ],
+  [
+    "Better than yesterday",
+    "Same as yesterday",
+    "Worse than yesterday"
+  ],
+  [
+    'A phone call',
+    'A visit',
+    'Nothing! I\'m good today :)'
+  ],
 ];
